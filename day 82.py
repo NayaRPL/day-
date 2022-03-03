0734fcea0519698f5bf30cd9107c5e59284ab0c1
NayaRPL <naya.mmj@gmail.com>
2022-03-03 21:59:51 +0800

    day 82

---
+++ day 82.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        
+    def prepend(self,node):
+        node.next = self.head
+        self.head = node
+    def print_all(self):
+        node = self.head
+        while node is not None:
+            print(node.value)
+            node = node.next
+            
+list = LinkedList()
+
+list.prepend(Node(1))  
+list.prepend(Node(2)) 
+list.prepend(Node(3))
+list.prepend(Node(4))
+list.prepend(Node(5))     
+list.print_all()
+        
